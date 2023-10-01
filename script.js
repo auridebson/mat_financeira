14ed25501caabde311d225f04e4c5a2befa15e7f
Auridebson Sousa <auridebson@vutus.com>
2023-10-01 10:44:10 -0300

    Update

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function calcJurosSimples() {
     const nrCapital = document.querySelector("#nrCapital").value
     const nrTaxa = document.querySelector("#nrTaxa").value
     const nrMontante = document.querySelector("#nrMontante").value
+    const nrTempo = document.querySelector("#nrTempo").value
 
     if (nrCapital === "") {
         alert(`Capital sem preenchimento de valor.\n${nrCapital}`)
@@ -13,7 +14,11 @@ function calcJurosSimples() {
     } else
     if (nrMontante === "") {
         alert(`Capital e Taxa preenchidos e montante sem preenchimento.\n${nrCapital}\n${nrTaxa}\n${nrMontante} `)
-    } else {
+    } else
+    if (nrTempo === "") {
+        alert(`Capital,Taxa, Montante e tempo sem preenchimento.\n${nrCapital}\n${nrTaxa}\n${nrMontante}\n${nrTempo} `)
+    }
+    else {
         alert("INFO:\nVocê já tem todas as informações da questão.")
     }
     
